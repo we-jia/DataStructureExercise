7b1a7f34d4502595fe6f7575068fba546ea1cda9
weJia <lanwei125531@gmail.com>
2021-09-06 19:12:11 +0800

    add BST delete、sort method

--- src/tree/BinarySearchTree.java
+++ src/tree/BinarySearchTree.java
@@ -40,4 +40,62 @@ public class BinarySearchTree {
 			return node;
 		}
 	}
+
+	// inorder-traversal
+	public void printSortedResult() {
+		inorderTraversal(root);
+	}
+
+	public void inorderTraversal(TreeNode node) {
+		if (node == null) {
+			return;
+		}
+		inorderTraversal(node.leftNode);
+		System.out.println(node.value);
+		inorderTraversal(node.rightNode);
+	}
+
+	public int successor(TreeNode node) {
+		node = node.rightNode;
+		while (node.leftNode != null) {
+			node = node.leftNode;
+		}
+		return node.value;
+	}
+
+	public int predecessor(TreeNode node) {
+		node = node.leftNode;
+		while (node.rightNode != null) {
+			node = node.rightNode;
+		}
+		return node.value;
+	}
+
+	public TreeNode delete(TreeNode node, int value) {
+		if (node == null) {
+			return null;
+		}
+
+		if (value > node.value) {
+			node.rightNode = delete(node.rightNode, value);
+		} else if (value < node.value) {
+			node.leftNode = delete(node.leftNode, value);
+		} else {
+			if (node.leftNode == null && node.rightNode == null) {
+				node = null;
+			} else if (node.rightNode != null) {
+				node.value = successor(node);
+				node.rightNode = delete(node.rightNode, node.value);
+			} else {
+				node.value = predecessor(node);
+				node.leftNode = delete(node.leftNode, node.value);
+			}
+		}
+
+		return node;
+	}
+
+	public void delete(int value) {
+		this.root = delete(this.root, value);
+	}
 }
--- src/tree/TestBinarySearchTree.java
+++ src/tree/TestBinarySearchTree.java
@@ -1,5 +1,19 @@
 package tree;
 
 public class TestBinarySearchTree {
-
+	public static void main(String[] args) {
+		BinarySearchTree tree = new BinarySearchTree();
+		
+		tree.add(1);
+		tree.add(5);
+		tree.add(2);
+		tree.add(3);
+		tree.add(7);
+		tree.add(9);
+		tree.printSortedResult();
+		
+		System.out.println("-----");
+		tree.delete(3);
+		tree.printSortedResult();
+	}
 }
